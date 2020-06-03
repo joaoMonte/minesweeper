9ab75c7918de04d6eaf301e3e9e523c0b9c5136c
joao.monte <joao.monte@gprt.ufpe.br>
2020-06-02 22:25:50 -0300

    Added a new variable isFlag to Cell class. Changed Field class to FieldCreator and added a new private variable called field

--- minesweeper.c
+++ minesweeper.c
@@ -5,23 +5,29 @@ using namespace std;
 class Cell {
 	public:
 		bool revealed;
+		bool isFlag;
 		bool isBomb;
 		Cell(bool isbomb){
 			isBomb = isbomb;
 			revealed = false;
+			isFlag = false;
 		}
 
 };
 
 
-class Field {
+class FieldCreator {
 	public:
-		int lines;
-		int columns;
 		Field(int c, int l){
 			lines = l;
 			columns = c;
+			field = new Cell[l][c]
 		}
+
+	private:
+		int lines;
+		int columns;
+		Cell* field;
 
 }
 
