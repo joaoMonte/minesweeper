0e829136e1de64f9797ea69bd939b6373d8a42da
joao.monte <joao.monte@gprt.ufpe.br>
2020-06-03 15:27:51 -0300

    Finished the method to print the field. Implemented a method to cast ints to str. Added, also, a simple queue class

--- minesweeper.c
+++ minesweeper.c
@@ -2,9 +2,31 @@
 #include <iterator> 
 #include <map> 
 #include <string> 
+#include <vector> 
+#include <sstream>
 
 
 using namespace std; 
+
+class Queue {
+	public:
+		void enqueue(string cellCoordinates){
+			buffer.push_back(cellCoordinates);
+		}
+
+		void dequeue(){
+			string auxiliaryString = buffer[0];
+			buffer.erase(buffer.begin());
+			return auxiliaryString;
+		}
+
+		bool isEmpty(){
+			return buffer.size() == 0
+		}
+
+	private:
+		vector<string> buffer;
+}
 
 class Cell {
 	public:
@@ -22,17 +44,25 @@ class Cell {
 
 class FieldManager {
 	public:
+
 		FieldManager(int c, int l){
 			lines = l;
 			columns = c;
 		}
 
+		string toStr(int number){
+			string auxiliaryString;
+			stringstream strstream;
+			strstream << number;
+			return strstream.str();
+		}
+
 		void initializeField(){
 			string key;
-			for (int i = 1; i <= l; i++){
-				for (int j = 1; j <= c; j++){
-					key = to_string(l) + "-" + to_string(c)
-					field.insert(pair<key, Cell>(1, Cell(false))); 
+			for (int i = 1; i <= lines; i++){
+				for (int j = 1; j <= columns; j++){
+					key = toStr(lines) + "-" + toStr(columns)
+					field.insert(pair<string, Cell>(key, Cell(false))); 
 				}
 			}
 		}
@@ -51,7 +81,7 @@ class FieldManager {
 		}
 
 		Cell getCellByCoordinates(int line, int column){
-				string key = to_string(line) + "-" + to_string(column)
+				string key = toStr(line) + "-" + toStr(column)
 				map<string, Cell>::iterator itr;
 				itr = field.find(key);
 				return itr->second;
@@ -68,13 +98,16 @@ class FieldManager {
 
 int main() 
 {
-	Cell t1 = Cell(false);
-	map<int, Cell> teste; 
-	map<int, Cell>::iterator itr;
+	FieldManager teste = FieldManager(5, 5);
+	teste.initializeField();
+	teste.printField();
+	//Cell t1 = Cell(false);
+	//map<int, Cell> teste; 
+	//map<int, Cell>::iterator itr;
 	
-	teste.insert(pair<int, Cell>(1, t1)); 
-	itr = teste.find(1);
-	cout << itr->second.isBomb;
+	//teste.insert(pair<int, Cell>(1, t1)); 
+	//itr = teste.find(1);
+	//cout << itr->second.isBomb;
 
 	return 0;
 } 
