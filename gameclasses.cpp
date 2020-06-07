7b08a917f1bbc92492890d6f35e6cc3440349d22
joao.monte <joao.monte@gprt.ufpe.br>
2020-06-07 11:56:15 -0300

    Added the implementation of cell methods

--- gameclasses.cpp
+++ gameclasses.cpp
@@ -8,17 +8,51 @@ using namespace std;
 
 
 void Queue :: enqueue(string cellCoordinates){
-		buffer.push_back(cellCoordinates);
+	buffer.push_back(cellCoordinates);
 }
 
 string Queue :: dequeue(){
-			string auxiliaryString = buffer[0];
-			buffer.erase(buffer.begin());
-			return auxiliaryString;
+	string auxiliaryString = buffer[0];
+	buffer.erase(buffer.begin());
+	return auxiliaryString;
 }
 
 bool Queue :: isEmpty(){
-			return buffer.size() == 0;
+	return buffer.size() == 0;
+}
+
+Cell :: Cell (bool isbomb)
+    :isBomb(isbomb), revealed(false), isFlag(false), bombsInNeighborhood(0)
+{}
+
+void Cell :: reveal(){
+	revealed = true;
+}
+
+bool Cell :: alreadyRevealed(){
+	return revealed;
+}
+
+bool Cell :: getIsBomb(){
+	return isBomb;
+}
+
+void Cell :: setFlag(){
+	isFlag = true;
+}
+
+bool Cell :: getIsFlag(){
+	return isFlag;
+}
+
+void Cell :: incrementBombsInNeighborhood(){
+	if (!isBomb) {
+		bombsInNeighborhood++;
+	}
+}
+
+int Cell :: getBombsInNeighborhood(){
+	return bombsInNeighborhood;
 }
 
 
